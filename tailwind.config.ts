dc23127a52642166838ec79ed678523cd3491492
Manozsen <maanozsen@gmail.com>
2026-04-29 23:57:16 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,16 +3,16 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         mint: "#00FFA3",
+        obsidian: "#0A0A0A",
       },
       fontFamily: {
-        jakarta: ["var(--font-jakarta)"],
+        sans: ["var(--font-jakarta)", "sans-serif"],
       },
     },
   },
