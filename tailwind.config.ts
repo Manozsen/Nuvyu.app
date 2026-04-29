47a7f608f8a7a5cedcd636eda885a9c5ed2bf4f9
Manozsen <maanozsen@gmail.com>
2026-04-29 23:37:01 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,12 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // Extra safety
   ],
   theme: {
     extend: {
       colors: {
         mint: "#00FFA3",
-        obsidian: "#050505",
       },
     },
   },
