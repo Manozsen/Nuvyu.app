9f889f21d91e1c4007a19feb879bf22219668521
Manozsen <maanozsen@gmail.com>
2026-04-29 23:58:45 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Jab koi main URL open karega, wo automatically dashboard par chala jayega
-  redirect('/dashboard');
+  redirect("/dashboard");
 }
