1bc7e8c758952839fc4a303ed4c2aae3f40d2aa2
Manozsen <maanozsen@gmail.com>
2026-04-30 00:06:31 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    // Agar folder 'src' ke andar hai (Most likely yahi issue hai)
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // Agar folder seedha bahar hai
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
