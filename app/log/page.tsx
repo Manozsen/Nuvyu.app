bfba3103fca9cfb52563328c106f89f349fa0ccc
Manozsen <274403720+Manozsen@users.noreply.github.com>
2026-05-05 16:12:01 +0530

    Update page.tsx

--- app/log/page.tsx
+++ app/log/page.tsx
@@ -425,6 +425,7 @@ export default function LogActivity() {
               </motion.div>
             )}
           </AnimatePresence>
+           </div>
 
       <AnimatePresence>
         {showConfirm && (
